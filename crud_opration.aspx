dda20129cbf058dc5d0adf27ae8cb5ef7b879acb
manishjai122 <134128258+manishjai122@users.noreply.github.com>
2023-05-30 12:01:47 +0530

    frist submit

---
+++ crud_opration.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="crud_opration.aspx.cs" Inherits="practice.crud_opration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <%--        <div>
+            <asp:Label ID="lblempid" runat="server" Text="Empid:"></asp:Label>
+            <asp:TextBox ID="txtempid" runat="server"></asp:TextBox>
+        </div>--%>
+        <div>
+            <asp:Label ID="lblname" runat="server" Text="First Name:"></asp:Label>
+            <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="lbllname" runat="server" Text="Last Name:"></asp:Label>
+            <asp:TextBox ID="txtlname" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="lbldepart" runat="server" Text="Department:"></asp:Label>
+            <asp:DropDownList ID="dropdep" runat="server" AutoPostBack="True" DataSourceID="SqlDataSource1" DataTextField="depname" DataValueField="depname">
+                <asp:ListItem Selected="True">Select Department</asp:ListItem>
+                <asp:ListItem>Hr</asp:ListItem>
+                <asp:ListItem>Devoloper</asp:ListItem>
+                <asp:ListItem>Project manger</asp:ListItem>
+            </asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:employeeConnectionString %>" SelectCommand="SELECT * FROM [dptname]"></asp:SqlDataSource>
+        </div>
+        <div>
+            <asp:Label ID="lbljoindt" runat="server" Text="Joining Date:"></asp:Label>
+            <asp:TextBox ID="txtjoindt" runat="server" TextMode="Date"></asp:TextBox>
+        </div>
+        <br />
+        <div>
+            <asp:Label ID="lblgen" runat="server" Text="Gander:"></asp:Label>
+            <asp:RadioButtonList ID="chkgender" runat="server" RepeatDirection="Horizontal" TextAlign="Right">
+                <asp:ListItem>Male</asp:ListItem>
+                <asp:ListItem>Female</asp:ListItem>
+            </asp:RadioButtonList>
+        </div>
+        <br />
+        <br />
+        <div>
+            <asp:Button ID="btnadd" runat="server" Text="Insert" OnClick="btnadd_Click" />
+            &nbsp;
+            &nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" Text="Update" OnClick="Button1_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click" OnClientClick="return conform (&quot;Are You Sure To Deleted ?&quot;)" />
+            <br />
+            <br />
+        </div>
+        <asp:GridView ID="GridView1" runat="server" DataKeyNames="empid" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" ButtonType="Button" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ crud_opration.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace practice
+{
+    public partial class crud_opration : System.Web.UI.Page
+    {
+        string mycon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                clear();
+                filldata();
+            }
+        }
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+            string firstname = txtname.Text;
+            string lastname = txtlname.Text;
+            string department = dropdep.SelectedValue;
+            string joiningdate = txtjoindt.Text;
+            DateTime date2 = Convert.ToDateTime(joiningdate,
+            System.Globalization.CultureInfo.GetCultureInfo("hi-IN").DateTimeFormat);
+            string gender = chkgender.SelectedValue;
+            SqlConnection obj = new SqlConnection(mycon);
+            obj.Open();
+            SqlCommand cmd = new SqlCommand("employeeadd '" + firstname + "','" + lastname + "','" + department + "','" + joiningdate + "','" + gender + "'", obj);
+            cmd.ExecuteNonQuery();
+            obj.Close();
+            Response.Write("<script LANGUAGE='JavaScript' >alert('Successfully Save....')</script>");
+            clear();
+            filldata();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.SelectedValue);
+            string firstname = txtname.Text;
+            string lastname = txtlname.Text;
+            string department = dropdep.SelectedValue;
+            string joiningdate = txtjoindt.Text;
+            DateTime date2 = Convert.ToDateTime(joiningdate,
+                System.Globalization.CultureInfo.GetCultureInfo("hi-IN").DateTimeFormat);
+            string gender = chkgender.SelectedValue;
+            SqlConnection obj = new SqlConnection(mycon);
+            obj.Open();
+            SqlCommand cmd = new SqlCommand("employee_Update '"+ id +"','" + firstname + "','" + lastname + "','" + department + "','" + joiningdate + "','" + gender + "'", obj);
+            cmd.ExecuteNonQuery();
+            obj.Close();
+            Response.Write("<script LANGUAGE='JavaScript' >alert('Record Successfully Updated....')</script>");
+            clear();
+            filldata();
+
+        }
+        public void filldata()
+        {
+            SqlConnection obj = new SqlConnection(mycon);
+            SqlCommand cmd = new SqlCommand("Select * from crudopration", obj);
+            obj.Open();
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            obj.Close();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.SelectedValue);
+            SqlConnection obj = new SqlConnection(mycon);
+            obj.Open();
+            SqlCommand cmd = new SqlCommand("delete_Emp '" + id + "'", obj);
+            cmd.ExecuteNonQuery();
+            obj.Close();
+            Response.Write("<script LANGUAE='JavaScript' >alert('Record Successfully Deleted....')</script>");
+            clear();
+            filldata();
+
+
+        }
+        public void clear()
+        {
+            txtname.Text = "";
+            txtlname.Text = "";
+            txtjoindt.Text = "";
+            dropdep.SelectedIndex = -1;
+            chkgender.Items[0].Selected = false;
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.SelectedValue);
+            SqlConnection sql = new SqlConnection(mycon);
+            sql.Open();
+            SqlCommand cmd = new SqlCommand("select * from crudopration where empid = '" + id + "'", sql);
+
+            SqlDataReader dar = cmd.ExecuteReader();
+
+            while (dar.Read())
+            {
+                txtname.Text = dar["firstname"].ToString();
+                txtlname.Text = dar["Lastname"].ToString();
+                dropdep.SelectedValue = dropdep.Items.FindByText(dar["Department"].ToString()).Text;
+                txtjoindt.Text = dar["joiningdate"].ToString();
+                chkgender.SelectedValue = dar["gender"].ToString();
+            }
+        }
+        public void DataFound()
+        {
+            string col = "";
+            int val = 10;
+            SqlConnection sql = new SqlConnection(mycon);
+            sql.Open();
+            SqlCommand cmd = new SqlCommand("select empid from crudopration where empid = '" + val + "'", sql);
+
+            SqlDataReader dar = cmd.ExecuteReader();
+
+
+            while (dar.Read())
+            {
+
+                col = col + dar["empid"];
+            }
+            Response.Redirect(col);
+
+        }
+    }
+}
